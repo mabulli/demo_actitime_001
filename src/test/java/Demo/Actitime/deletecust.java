dfa636523326bc751d1d0341947db68a43c88006
mabulli <mabullip@gmail.com>
2023-07-27 20:04:07 +0530

    second round

---
+++ src/test/java/Demo/Actitime/deletecust.java
@@ -0,0 +1,10 @@
+package Demo.Actitime;
+
+import org.testng.annotations.Test;
+
+public class deletecust {
+  @Test
+  public void deletecustomer() {
+	  System.out.println("Delete customer");
+  }
+}
